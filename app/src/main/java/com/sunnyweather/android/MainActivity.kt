d494ccf20ad806d767e3572f297a6239edd73b98
wangzp <2441961052@qq.com>
2022-06-23 22:37:09 +0800

    v.0.01

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sunnyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.liveData
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
@@ -11,5 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("TX","HELLO WORLD")
     }
 }
